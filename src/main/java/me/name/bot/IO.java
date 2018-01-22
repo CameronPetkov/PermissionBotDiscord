95fbd006723b39210907b494d24d59d51b2af38d
Cameron Petkov <Cameron.Petkov@outlook.com>
2018-01-22 18:09:19 +0800

    Moved logging of user's command to other methods, so it could be DM'd and they can easily refer to the command they used.

--- src/main/java/me/name/bot/IO.java
+++ src/main/java/me/name/bot/IO.java
@@ -4,30 +4,6 @@ import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import java.io.*;
 
 public class IO {
-    public void write(String time, CommandEvent command) {
-        FileOutputStream fileStrm = null;
-        PrintWriter pw;
-
-        try {
-            fileStrm = new FileOutputStream("DiscordBotCommandUsage.txt", true);
-            pw = new PrintWriter(fileStrm);
-
-            String author = command.getMember().getEffectiveName();
-            String message = command.getMessage().getContentDisplay();
-
-            pw.println(time + " - " + author + ": " + message);
-            pw.close();
-        }
-        catch (IOException e) {
-            if (fileStrm != null) {
-                try {
-                    fileStrm.close();
-                }
-                catch (IOException e2) {}
-            }
-        }
-    }
-
     public void write(String msg) {
         FileOutputStream fileStrm = null;
         PrintWriter pw;
