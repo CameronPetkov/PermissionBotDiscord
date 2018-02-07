80be34838a01bed14bb9673accc548dc39e1887a
Cameron Petkov <Cameron.Petkov@outlook.com>
2018-02-07 23:58:28 +0800

    Revert: Remove depreciated method call for setting Bot status

--- src/main/java/me/name/bot/Bot.java
+++ src/main/java/me/name/bot/Bot.java
@@ -1,32 +1,33 @@
 package me.name.bot;
 
-        import com.jagrosh.jdautilities.commandclient.CommandClient;
-        import com.jagrosh.jdautilities.commandclient.CommandClientBuilder;
-        import me.name.bot.Commands.CourseSelect;
-        import me.name.bot.Commands.Enrolment;
-        import me.name.bot.Commands.Unenrolment;
-        import me.name.bot.Commands.Helper;
-        import me.name.bot.Common.JSONLoad;
-        import me.name.bot.Models.Configuration;
-        import net.dv8tion.jda.core.AccountType;
-        import net.dv8tion.jda.core.JDABuilder;
-        import net.dv8tion.jda.core.entities.Game;
-
+import com.jagrosh.jdautilities.commandclient.CommandClient;
+import com.jagrosh.jdautilities.commandclient.CommandClientBuilder;
+import me.name.bot.Commands.CourseSelect;
+import me.name.bot.Commands.Enrolment;
+import me.name.bot.Commands.Unenrolment;
+import me.name.bot.Commands.Helper;
+import me.name.bot.Common.JSONLoad;
+import me.name.bot.Models.Configuration;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 
 public class Bot {
     public static void main(String[] arguments) throws Exception {
-        CommandClientBuilder builder = new CommandClientBuilder();
         Configuration config = JSONLoad.LoadJSON("data/config.json", Configuration.class);
 
-        builder.setOwnerId(config.getOwnerID()); //my userid
-        builder.setPrefix("!");  //!help, !command
-        builder.addCommands(new Enrolment(), new Unenrolment(), new CourseSelect(), new Helper()); //4 commands
+        CommandClientBuilder builder = new CommandClientBuilder();
 
-        CommandClient client = builder.build();
+        builder.setOwnerId(config.getOwnerID()); //user-id
+        builder.setPrefix("!");
+        builder.addCommands(new Enrolment(), new Unenrolment(), new CourseSelect(), new Helper());
+        builder.setGame(null);
 
         JDABuilder api = new JDABuilder(AccountType.BOT);
-        api.setGame(Game.streaming("Videos of blonde chicks", "www.blondegirls.com"));
         api.setToken(config.getToken()); //token-id, not to be released
+        api.setGame(Game.watching(config.getGame()));
+        CommandClient client = builder.build();
+
         api.addEventListener(client);
         api.buildAsync();
     }
