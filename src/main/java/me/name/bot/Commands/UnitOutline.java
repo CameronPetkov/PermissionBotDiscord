750a59e9cd392543c9bab75d97ecf7146b8d9749
Cameron Petkov <Cameron.Petkov@outlook.com>
2018-02-13 23:11:49 +0800

    Unit outline is fetched from argument OR given no argument it tries to fetch based on channel name, and saves in projectDir/output/unitOutlines

--- src/main/java/me/name/bot/Commands/UnitOutline.java
+++ src/main/java/me/name/bot/Commands/UnitOutline.java
@@ -6,12 +6,16 @@ import me.name.bot.Common.EnrolmentHelper;
 import me.name.bot.Common.JSONLoad;
 import me.name.bot.Models.Configuration;
 import me.name.bot.Models.Unit;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import org.apache.commons.lang3.text.WordUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -39,67 +43,95 @@ public class UnitOutline extends Command {
         Unit[] units = JSONLoad.LoadJSON("data/units.json", Unit[].class);   //load JSON
 
         String arg = args[0].trim().toLowerCase();
-        foundUnit = Arrays.stream(units).filter(x -> Arrays.stream(x.getAbbreviation()).anyMatch(z -> z.equalsIgnoreCase(arg))
-                || x.getFullName().equalsIgnoreCase(arg) || x.getUnitCode().equalsIgnoreCase(arg)).findFirst().orElse(null);
-        if(foundUnit != null) {
-            String unitcode = foundUnit.getUnitCode();
 
-            System.setProperty("webdriver.gecko.driver", "E:\\Libraries\\Downloads\\geckodriver.exe");
-            Configuration config = JSONLoad.LoadJSON("data/config.json", Configuration.class);
+        if(arg == null || arg.isEmpty()) {
+            String name = event.getChannel().getName();
+            foundUnit = Arrays.stream(units).filter(x -> Arrays.stream(x.getAbbreviation()).anyMatch(z -> z.equalsIgnoreCase(name))).findFirst().orElse(null);
+            if(foundUnit != null) {
+                String unitcode = foundUnit.getUnitCode();
+                checkUnit(unitcode, event, foundUnit);
+            }
+            else {
+                event.replyInDm("Unit code/name needs to start with a letter.");
+            }
+        }
+        else {
+            foundUnit = Arrays.stream(units).filter(x -> Arrays.stream(x.getAbbreviation()).anyMatch(z -> z.equalsIgnoreCase(arg))
+                    || x.getFullName().equalsIgnoreCase(arg) || x.getUnitCode().equalsIgnoreCase(arg)).findFirst().orElse(null);
+            if (foundUnit != null) {
+                String unitcode = foundUnit.getUnitCode();
+                checkUnit(unitcode, event, foundUnit);
+            } else {
+                EnrolmentHelper.giveErrorMessage(arg, event);
+            }
+        }
+    }
 
-            FirefoxDriver driver = new FirefoxDriver();
-            driver.get("https://ctl.curtin.edu.au/teaching_learning_services/unit_outline_builder/search_published_UO.cfm");
-            WebElement id = driver.findElement(By.xpath("//input[@name='username']"));
-            id.sendKeys(config.getStudentID());
-            WebElement pw = driver.findElement(By.xpath("//input[@name='password']"));
-            pw.sendKeys(config.getStudentPassword());
-            WebElement button = driver.findElement(By.xpath("//input[@name='loginSubmit']"));
-            button.click();
+    private void checkUnit(String unitcode, CommandEvent event, Unit foundUnit) {
+        System.setProperty("webdriver.gecko.driver", "E:\\Libraries\\Downloads\\geckodriver.exe");
+        Configuration config = JSONLoad.LoadJSON("data/config.json", Configuration.class);
 
-            WebDriverWait wdw = new WebDriverWait(driver, 40, 500);
-            wdw.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='unitCode']")));
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://ctl.curtin.edu.au/teaching_learning_services/unit_outline_builder/search_published_UO.cfm");
+        WebElement id = driver.findElement(By.xpath("//input[@name='username']"));
+        id.sendKeys(config.getStudentID());
+        WebElement pw = driver.findElement(By.xpath("//input[@name='password']"));
+        pw.sendKeys(config.getStudentPassword());
+        WebElement button = driver.findElement(By.xpath("//input[@name='loginSubmit']"));
+        button.click();
 
-            WebElement unit = driver.findElement(By.xpath("//input[@name='unitCode']"));
-            unit.sendKeys(unitcode);
-            WebElement button2 = driver.findElement(By.xpath("//input[@name='next']"));
-            button2.click();
+        WebDriverWait wdw = new WebDriverWait(driver, 40, 500);
+        wdw.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='unitCode']")));
 
-            int currYear = Year.now().getValue();
-            wdw.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("thead")));
+        WebElement unit = driver.findElement(By.xpath("//input[@name='unitCode']"));
+        unit.sendKeys(unitcode);
+        WebElement button2 = driver.findElement(By.xpath("//input[@name='next']"));
+        button2.click();
 
-            int rowCount = driver.findElements(By.xpath("//table[@class='fullwidth']/tbody/tr")).size();
-            // int colCount = driver.findElements(By.xpath("//table[@class='fullwidth']/thead/tr/th")).size();
+        int currYear = Year.now().getValue();
+        wdw.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("thead")));
 
-            int maxYear = 0;
-            boolean exactMatch = false;
-            WebElement linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[2]/td[5]"));
-            for(int ii=1; ii<=rowCount; ii++) {
-                if(driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[3]")).getText().equals("Bentley Campus")) {
-                    WebElement yearElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[5]"));
-                    int year = Integer.parseInt(yearElement.getText());
-                    if(exactMatch) { }
-                    else if(year-currYear == 0) {
-                        exactMatch = true;
-                        linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[2]"));
-                    }
-                    else {
-                        if(year>maxYear) {
-                            maxYear = year;
-                            linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[2]/a"));
-                        }
+        int rowCount = driver.findElements(By.xpath("//table[@class='fullwidth']/tbody/tr")).size();
+        // int colCount = driver.findElements(By.xpath("//table[@class='fullwidth']/thead/tr/th")).size();
+
+        int maxYear = 0;
+        boolean exactMatch = false;
+        WebElement linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[2]/td[5]"));
+        for(int ii=1; ii<=rowCount; ii++) {
+            if(driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[3]")).getText().equals("Bentley Campus")) {
+                WebElement yearElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[5]"));
+                int year = Integer.parseInt(yearElement.getText());
+                if(exactMatch) { }
+                else if(year-currYear == 0) {
+                    exactMatch = true;
+                    linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[2]"));
+                }
+                else {
+                    if(year>maxYear) {
+                        maxYear = year;
+                        linkElement = driver.findElement(By.xpath("//table[@class='fullwidth']/tbody/tr[" + ii + "]/td[2]/a"));
                     }
                 }
             }
-
-            String link = linkElement.getAttribute("href");
-            event.replyInDm(link);
-            try {
-                URL url = new URL(link);
-                InputStream in = url.openStream();
-                Files.copy(in, Paths.get(unitcode.toUpperCase()+".pdf"), StandardCopyOption.REPLACE_EXISTING);
-                in.close();
-            } catch (MalformedURLException e) { }
-            catch (IOException e) { }
         }
+
+        String link = linkElement.getAttribute("href");
+        driver.close();
+
+        String saveDir = "output/unitOutlines";
+        try {
+            Files.createDirectory(Paths.get(saveDir));
+        } catch (IOException e) {  }
+
+        try {
+            URL url = new URL(link);
+            InputStream in = url.openStream();
+            Files.copy(in, Paths.get(saveDir + "\\"+ unitcode.toUpperCase()+".pdf"), StandardCopyOption.REPLACE_EXISTING);
+            in.close();
+        } catch (MalformedURLException e) { }
+        catch (IOException e) { }
+
+        Message message = new MessageBuilder().append("Unit outline for " + WordUtils.capitalize(foundUnit.getFullName()) + ": ").build();
+        event.getChannel().sendFile(new File(saveDir + "\\" + unitcode.toUpperCase()+".pdf"), message).queue();
     }
 }
